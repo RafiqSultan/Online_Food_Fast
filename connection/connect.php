653199fa088829910f5a24345cab2e08341bcc99
RafiqSultan <Rafiqaasultan@gmail.com>
2022-06-28 15:50:44 -0700

    CREATE Web FrontEnd page

--- connection/connect.php
+++ connection/connect.php
@@ -1,12 +1,13 @@
 <?php
+// main connection in frontend and admin
 $serverName="localhost";
 $userName="root";
 $password="";
 $db_Name="online-food-delivery";
-
+// Craete connection with database
 $connection=mysqli_connect($serverName,$userName,$password,$db_Name);
+//Check connection 
 if(!$connection){
     echo " ERROR ON ACCESSING DATABASE".mysqli_connect_error();
 }
-echo "<h1>Successfull Accessing</h1>"
 ?>
--- php/register.php
+++ php/register.php
@@ -1,6 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+session_start();
+include('../connection/connect.php');
+if(isset($_POST['submit'])){
+$userName=$_POST['username'];
+$fullName=$_POST['fullname'];
+$userEmail=$_POST['email'];
+$userPhone=$_POST['phone'];
+$userPassword=$_POST['password'];
+$userConfirmPassword=$_POST['cpassword'];
+$userDeliveryAddresss=$_POST['address'];
+
+    if(empty($userName) && empty($fullName) &&empty($userEmail) && empty($userPassword) && empty($userConfirmPassword))
+    {
+        echo "<Script>alertify.success('Success message');</Script> ";
+    }
+    else{
+        
+    }
+    
+
+    
+}
+
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,9 +35,13 @@
     <link rel="icon" href="../images/img/pimg.jpg">
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/font-awesome.min.css" rel="stylesheet">
-    <!-- <link href="css/animsition.min.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet"> -->
-    <link rel="stylesheet" href="../css/login.css">
+    <!-- <link href="../css/animsition.min.css" rel="stylesheet">
+    <link href="../css/animate.css" rel="stylesheet"> -->
+    <!-- Alert Notifction  -->
+    <link  href="../css/alertify.css" rel="stylesheet">
+    <link  href="../css/alertify.default.css" rel="stylesheet">
+    <link  href="../css/login.css" rel="stylesheet">
+    <link  href="../css/login.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
 </head>
 
@@ -42,52 +72,50 @@
 
         </header>
 
+
         <div class="page-wrapper">
             <section class="contact-page inner-page">
                 <div class="container ">
-                    <div class="row ">
+                    <div class="row">
+                    
                         <div class="col-md-12">
+                        <div class="register-form">
                             <div class="widget">
                                 <div class="widget-body">
-
                                     <form action="" method="post">
                                         <div class="row">
-                                            <div class="form-group col-sm-6">
-                                                <label for="exampleInputEmail1">User-Name</label>
-                                                <input class="form-control" type="text" name="username" id="example-text-input">
+                                            <div class="form-group col-sm-6 ">
+                                                <label for="exampleInputEmail1">Name:</label>
+                                                <input class="form-control" type="text" name="nam" id="example-text-input"placeholder="User Name" require>
+                                            
                                             </div>
-                                            <div class="form-group col-sm-6">
-                                                <label for="exampleInputEmail1">Full Name</label>
-                                                <input class="form-control" type="text" name="fullname" id="example-text-input">
-                                            </div>
+                                            
 
                                             <div class="form-group col-sm-6">
-                                                <label for="exampleInputEmail1">Email Address</label>
-                                                <input type="text" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                                <label for="exampleInputEmail1">Email: </label>
+                                                <input type="text" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp"placeholder="Email" require>
                                             </div>
                                             <div class="form-group col-sm-6">
-                                                <label for="exampleInputEmail1">Phone number</label>
-                                                <input class="form-control" type="text" name="phone" id="example-tel-input-3">
+                                                <label for="exampleInputPassword1">Password:</label>
+                                                <input type="password" class="form-control" name="password" id="exampleInputPassword1"placeholder="Password" require>
                                             </div>
                                             <div class="form-group col-sm-6">
-                                                <label for="exampleInputPassword1">Password</label>
-                                                <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+                                                <label for="exampleInputPassword1">Confirm Password :</label>
+                                                <input type="password" class="form-control" name="cpassword" id="exampleInputPassword2" placeholder="Confirm Password" require>
                                             </div>
-                                            <div class="form-group col-sm-6">
-                                                <label for="exampleInputPassword1">Confirm password</label>
-                                                <input type="password" class="form-control" name="cpassword" id="exampleInputPassword2">
-                                            </div>
-                                            <div class="form-group col-sm-12">
-                                                <label for="exampleTextarea">Delivery Address</label>
-                                                <textarea class="form-control" id="exampleTextarea" name="address" rows="3"></textarea>
-                                            </div>
+                                           
 
                                         </div>
 
                                         <div class="row">
                                             <div class="col-sm-4">
-                                                <p> <input type="submit" value="Register" name="submit" class="btn theme-btn"> </p>
-                                                <small>Already have an Acount?</small>
+                                                <p> <input type="submit" value="Register" name="submit" class="btn"> </p>
+                                                
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <small>Already have an Acount? <a href="login.php">login</a></small>
                                             </div>
                                         </div>
                                     </form>
@@ -95,9 +123,9 @@
                                 </div>
 
                             </div>
-
+                            
                         </div>
-
+                        </div>
                     </div>
                 </div>
             </section>
@@ -105,14 +133,17 @@
     </div>
 
     <!-- *links of package or framwork js  -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/tether.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/animsition.min.js"></script>
-    <script src="js/bootstrap-slider.min.js"></script>
-    <script src="js/jquery.isotope.min.js"></script>
-    <script src="js/headroom.js"></script>
-    <script src="js/foodpicky.min.js"></script>
+     <!-- Alert Notifaction js -->
+     <script src="../js/alertify.min.js"></script>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/tether.min.js"></script>
+   
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/animsition.min.js"></script>
+    <script src="../js/bootstrap-slider.min.js"></script>
+    <script src="../js/jquery.isotope.min.js"></script>
+    <script src="../js/headroom.js"></script>
+    <script src="../js/foodpicky.min.js"></script>
 </body>
 
 </html>
